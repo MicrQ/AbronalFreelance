15f7942dd827f998a52b0f0d25025ccd68583969
MicrQ <mail.micrq@proton.me>
2024-07-22 12:35:50 +0300

    July 22. some changes made, razor componenets added. location api implemented and some more actions created

---
+++ Shared/DTOs/LocationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AbronalFreelance.Shared.DTOs;
+
+public class LocationDTO
+{
+    public string Name { get; set; }
+    public string LocationTypeId { get; set; }
+    public string? ParentId { get; set; }
+}
--- Shared/obj/Debug/net6.0/AbronalFreelance.Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net6.0/AbronalFreelance.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eaa6fcec4d0f213e69b786d73454697cb7a640f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e95431bf29383b4aa49618259d5b81f9d70b12e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
