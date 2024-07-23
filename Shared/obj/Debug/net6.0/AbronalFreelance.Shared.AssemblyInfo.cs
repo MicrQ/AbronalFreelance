5535138a3ee4d78dc9e6caeb5cfdaed7b17a9870
MicrQ <mail.micrq@proton.me>
2024-07-23 09:09:17 +0300

    July 23 final working status

--- Shared/obj/Debug/net6.0/AbronalFreelance.Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net6.0/AbronalFreelance.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b924ad4d8d8ff879ac5aee93a7b6968dc8b0883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce041e45d1000b36c8976566531d4ea293db8629")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
