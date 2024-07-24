63fa2df1103397883feadee33c6fc694a5d305a9
MicrQ <mail.micrq@proton.me>
2024-07-24 08:38:54 +0300

    July 24. Last working state

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fa817b7cf8a608afe2dc218b547c7c4b2ff1f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7fc8ce623ac22533ec65c97066e37a22ea3e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
