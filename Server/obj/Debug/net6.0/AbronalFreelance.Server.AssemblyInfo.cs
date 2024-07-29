6c39dbcb5ca5e959f10e0ef4f1bcaa3efa8a2d05
MicrQ <mail.micrq@proton.me>
2024-07-29 18:08:37 +0300

    final working state. working on the user profile options

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3bb6bc2865d391aacf4028c9a103aefed4ccdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f40e57dcbb5d8fa792acf21e3368fb2131ba28b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
