a0d0e69d735f1f1a7dd72e70241d2c2240984372
MicrQ <mail.micrq@proton.me>
2024-07-29 23:36:08 +0300

    last working state. profile page created

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3bb6bc2865d391aacf4028c9a103aefed4ccdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c39dbcb5ca5e959f10e0ef4f1bcaa3efa8a2d05")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
