043cdfffba37f2b4eabe98ddb659fb48844fb3ef
AbenetGebre <mail.micrq@proton.me>
2024-08-01 15:10:49 +0300

    working on the update operation on client side. retrived the data successfully. about to make the http request

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d939a6a42f1ca950fff03a20e57f0011c7f4aa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8aff29a3a0f589214313950202c21610bdbd67")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
