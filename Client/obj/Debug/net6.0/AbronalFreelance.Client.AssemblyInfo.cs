c539e3148308b27db25f0eeb9f3628943f44dbbe
AbenetGebre <mail.micrq@proton.me>
2024-08-01 17:20:01 +0300

    started to make the pages responsive

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccbcec3687c4a731efeabc5fceb5f0b87402afb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1410c4919807dc97cd5c4264ec8ebbd074a89aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
