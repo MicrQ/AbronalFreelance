7b4cff9a5322211dac09c5f816e0c40cfa7cb0ab
AbenetGebre <mail.micrq@proton.me>
2024-08-08 16:30:10 +0300

    frontend implemented for findfreelancer page. backend on its way

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7dc43c2883999477b4c5fa2ed0377f3633f8e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee7799df7f142db5bbd3fab484741f3e3f04275")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
