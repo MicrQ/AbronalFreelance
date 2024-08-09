7246da99cd6b3f6ecac58f52f5b427a474bca414
AbenetGebre <mail.micrq@proton.me>
2024-08-09 09:53:24 +0300

    template file added

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4cff9a5322211dac09c5f816e0c40cfa7cb0ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026957ef045f4fb7ccea200524ccc6812b9d48e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
