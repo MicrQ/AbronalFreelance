5c93a4bce498c6f1069ea281c846cc8c5de335d4
AbenetGebre <mail.micrq@proton.me>
2024-08-09 11:43:48 +0300

    copied all css files

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026957ef045f4fb7ccea200524ccc6812b9d48e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12b778f781dc5dd1d32e400cb7dcdb75c68dc4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
