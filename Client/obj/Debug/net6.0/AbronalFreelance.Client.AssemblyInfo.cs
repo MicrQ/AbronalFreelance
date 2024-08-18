d7609e4c71f3f22067d933addafd7967c9fdbbb1
AbenetGebre <mail.micrq@proton.me>
2024-08-18 22:56:21 +0300

    working on the new ui

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+453816eb30ddce1a869aebb988017f70becbe3bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acdefe9d730fdb00d8b4e1319cfae188c3a00de")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
