7f4d5c2c257032d75f3d3b2d67c0568d9fac47dc
AbenetGebre <mail.micrq@proton.me>
2024-08-20 17:33:47 +0300

    client profile setting page modified

--- Shared/DTOs/ClientProfileDTO.cs
+++ Shared/DTOs/ClientProfileDTO.cs
@@ -1,22 +1,21 @@
 namespace AbronalFreelance.Shared.DTOs;
 
-class ClientProfileDTO {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? UserName { get; set; }
+public class ClientProfileDTO {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
     public string? Headline { get; set; }
-    public string? Email { get; set; }
-    public string? Phone { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int LocationId { get; set; }
     // public string? Location { get; set; }
 
     //
 
     public string UserId { get; set; }
-    public int CompanyLocationId { get; set; }
-    public string CompanyName { get; set; } = string.Empty;
+    public string? CompanyName { get; set; } = string.Empty;
     public string? TinNo { get; set; }
-    public int? LocationId { get; set; }
-    public string? LocationString { get; set; }
+    public int? CompanyLocationId { get; set; }
     public DateTime? EstablishedDate { get; set; }
 
     public bool Flag { get; set; } = false;
