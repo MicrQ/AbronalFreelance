87a66982cf6b609280a8167127087d69e2ca337c
AbenetGebre <mail.micrq@proton.me>
2024-08-20 15:09:42 +0300

    working on the client profile setting

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60479b5c2782fa65e043fe9775b8b504ceb116df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f2da49e8801bd3458898ddb00e19152953af25")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
