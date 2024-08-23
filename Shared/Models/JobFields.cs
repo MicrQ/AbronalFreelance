a7173292b3d6ef88986dcf5cff5bb676d1edc45a
AbenetGebre <mail.micrq@proton.me>
2024-08-23 14:39:48 +0300

    final best working state

--- Shared/Models/JobFields.cs
+++ Shared/Models/JobFields.cs
@@ -4,7 +4,7 @@ public class JobFields
 {
     public int Id { get; set; }
     public int JobId { get; set; }
-    public Job Job { get; set; }
     public int FieldId { get; set; }
+    public Job Job { get; set; }
     public Field Field { get; set; }
 }
