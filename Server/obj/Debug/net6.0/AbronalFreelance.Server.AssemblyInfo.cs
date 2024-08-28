e4baf82e2eccced8d9cb9f6b0d3c4e76ab5258b0
AbenetGebre <mail.micrq@proton.me>
2024-08-28 17:44:08 +0300

    final best working state. working on job application form

--- Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/AbronalFreelance.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daab3981df71c63818310b89aea32b2ee4b3c1ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a820ef129336ea7870e2f2ec2ea979fd090757")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
