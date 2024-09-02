be245f1e6f2d94d8861b70cc2297e0a13dc8d511
AbenetGebre <mail.micrq@proton.me>
2024-09-02 11:10:28 +0300

    final best working state done with showing freelancer his own applications and their details

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f619701f40eb674d180ae3180b3148cc226e970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216eeb27b8ecf332c7aa8de797cbc69b7c9408b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
