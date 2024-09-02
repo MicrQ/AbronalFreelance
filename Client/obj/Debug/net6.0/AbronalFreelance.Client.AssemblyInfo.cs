80b1f611c9a16f4613a10a8db65be51d4d546599
AbenetGebre <mail.micrq@proton.me>
2024-09-02 12:13:48 +0300

    best working state. working on action of application

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04447e0349f8f24542fdeb23ca2ae747de277595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37d6c1a872f3b95059d34c676b2f17912ed098f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
