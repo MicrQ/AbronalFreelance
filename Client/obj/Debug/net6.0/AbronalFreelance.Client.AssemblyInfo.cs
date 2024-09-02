ed1d2463d9dbb44ab931a48dbded606b77f1033b
AbenetGebre <mail.micrq@proton.me>
2024-09-02 17:23:38 +0300

    created the conract creating page. working on the backend of it

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4946367fa25cfe66b5a319cc276b4d76506c86e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a239a1485f7e4868af10de6621c6e5978b3b3f0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shared/Models/Contract.cs
+++ Shared/Models/Contract.cs
@@ -4,9 +4,9 @@ public class Contract
 {
     public int Id { get; set; }
     public int ApplicationId { get; set; }
-    public Application Application { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public Application Application { get; set; }
 }
