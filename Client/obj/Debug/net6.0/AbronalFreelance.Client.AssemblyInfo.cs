38fefa45183de548eb4370a861e922bcb52314c1
AbenetGebre <mail.micrq@proton.me>
2024-09-03 11:25:09 +0300

    404 page completed

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d31ce75a56df162e4240cf5eaa7c47faf0785b72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b14775f51d8ac1152c4071c9794d6aa90fc0279")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
