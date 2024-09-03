34e3d515d1068b8c828e51c0f8b6449c5b4090e7
AbenetGebre <mail.micrq@proton.me>
2024-09-03 15:29:37 +0300

    Modified: done with displaying list of contracts for clients

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c63e0624b4eddf7979a4dcbc13dd7b636c8ba9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53bfbb5c087a20936882019d4aa4debf9a449984")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
