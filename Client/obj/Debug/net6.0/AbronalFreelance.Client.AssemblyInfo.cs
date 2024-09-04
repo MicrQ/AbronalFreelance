88a96dfb39d6f47a0d79c8ff3d0687cc1cd1c255
AbenetGebre <mail.micrq@proton.me>
2024-09-04 10:39:29 +0300

    diplayed dynamic freelancers on the find-freelancer page

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a70e1f0243c5cd68c9a7ebd47d8f12cc69eef52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cddbde463b8540f259b292d7bf69fdbf7a57a08f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
