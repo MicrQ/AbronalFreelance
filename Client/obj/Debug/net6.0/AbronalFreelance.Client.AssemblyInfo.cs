dfbc01f8d59e08919d6c66aa294a63e22c6e5954
AbenetGebre <mail.micrq@proton.me>
2024-09-05 17:07:18 +0300

    final best state

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce506099d9b13ee1ebb738f34fbcba18473b5de7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6845b77dde606f0920345b5ab343148e5c6b5a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
