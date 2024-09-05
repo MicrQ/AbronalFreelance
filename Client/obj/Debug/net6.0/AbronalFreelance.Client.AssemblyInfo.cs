567a92218717362f585ec359cc38c5127b9f58fe
AbenetGebre <mail.micrq@proton.me>
2024-09-05 08:49:18 +0300

    database backedup just in case the new database fails

--- Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net6.0/AbronalFreelance.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03bc397974bf8fa285ec115c6dedf298d5ee6134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5f9d244e072161000c996f9cbffb317912bd8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbronalFreelance.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
